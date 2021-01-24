df66885c5bb4b74bacf0a17014f88a3d91ca150a
Example <osman-95@hotmail.com>
2021-01-24 13:55:48 +0300

    Proje dosyası ekle.

---
+++ Odev4/MyDictionary.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Odev4
+{
+    class MyDictionary<TKey,TValue>
+    {
+        TKey[] tkey;
+        TValue[] tvalue;
+
+        public MyDictionary()
+        {
+            tkey = new TKey[0];
+            tvalue = new TValue[0];
+        }
+
+        public void Ekle(TKey itemKey, TValue itemValue)
+        {
+            int count = 0;
+            //Eğer itemKey daha önceden eklenmiş ise sayacı bir artırıyoruz.
+            for (int i = 0; i < tkey.Length; i++)
+            {
+                if (itemKey.Equals(tkey[i]))
+                {
+                    count++;
+                    break;
+                }
+
+            }
+            //Eğer sayac 0 ise itemKey daha önce eklenmemiştir.
+            if (count == 0)
+            {
+                TKey[] tempTKey = tkey;
+                TValue[] tempTValue = tvalue;
+
+                tkey = new TKey[tkey.Length + 1];
+                tvalue = new TValue[tvalue.Length + 1];
+
+                for (int i = 0; i < tempTKey.Length; i++)
+                {
+                    tkey[i] = tempTKey[i];
+                    tvalue[i] = tempTValue[i];
+                }
+
+                tkey[tkey.Length - 1] = itemKey;
+                tvalue[tkey.Length - 1] = itemValue;
+            }
+            //Eğer sayac 0 değil ise itemKey daha önce eklenmiştir.
+            else
+            {
+                Console.WriteLine("Bu itemKey zaten kullanılıyor.");
+            }
+            
+
+        }
+        //Tümünü listeler.
+        public void Listele()
+        {
+            for (int i = 0; i < tkey.Length; i++)
+            {
+                Console.WriteLine("Öğrenci Numarası : "+tkey[i]+" Adı Soyadı : "+tvalue[i]);
+            }
+        }
+        //Gönderilen itemKey eğer listede varsa onu listeler.
+        public void Listele(TKey itemKey)
+        {
+            int count = 0;
+            for (int i = 0; i < tkey.Length; i++)
+            {
+                if (itemKey.Equals(tkey[i]))
+                {
+                    Console.WriteLine("Öğrenci Numarası : " + tkey[i] + " Adı Soyadı : " + tvalue[i]);
+                    count++;
+                }
+            }
+
+            if (count.Equals(0))
+            {
+                Console.WriteLine("Kayıt Bulunamadı :"+count);
+            }
+        }
+
+
+    }
+}
---
+++ Odev4/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Odev4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MyDictionary<int, string> ogrenciler = new MyDictionary<int, string>();
+            ogrenciler.Ekle(1, "Osman Özkan");
+            ogrenciler.Ekle(2, "Ahmet Özkan");
+
+            //ogrenciler.Listele();
+
+            ogrenciler.Ekle(3, "Furkan Özkan");
+            ogrenciler.Ekle(4, "Emre Özkan");
+            //ogrenciler.Listele();
+
+            //ogrenciler.Ekle(1, "Yeni Öğrenci");
+            //ogrenciler.Listele();
+
+            ogrenciler.Listele(2);
+
+
+
+        }
+
+        
+    }
+}
